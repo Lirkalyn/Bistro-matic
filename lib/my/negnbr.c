ff85bb35bb34c7bbaf0c14b5ad4c263e08422b9b
alexandre1 <alexandre1.berthomier@epitech.eu>
2019-10-28 11:39:33 +0100

    [AD][ADD] change parser + [L] [UPDATE] SRC in Makefile have now fill.c, result.c, eval_expr.c, negnbr.c

---
+++ lib/my/negnbr.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2019
+** d
+** File description:
+** d
+*/
+
+#include <stdlib.h>
+#include "include/eval_expr.h"
+/*
+int isnum_neg(char *calc, int *num, int pos)
+{
+    int i;
+    int popo = 0;
+    if (calc[0] == '-')
+        num[popo] *= (-1);
+    for (i = 1; calc[i] != '\0'; i++) {
+        if (calc[i] >= '0' && calc[i] <= '9') {
+            if ((calc[(i - 1)] == '-') && (calc[(i - 2)] == '*'
+                || calc[(i - 2)] == '/' || calc[(i - 2)] == '+'
+                || calc[(i - 2)] == '-' || calc[(i - 2)] == '%')) {
+                if (num[0] < 0)
+                    num[popo] *= (-1);
+                else if (num[0] >= 0) {
+                    num[popo] *= (-1);
+                }
+                popo++;
+            }
+            else
+                popo++;
+            while (calc[i] >= '0' && calc[i] <= '9')
+                i++;
+            i--;
+        }
+    }
+}
+*/
