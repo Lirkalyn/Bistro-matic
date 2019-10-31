679cf436ecb0df5254aa4c84370091688685a950
Alexandre <alexandre1.berthomier@epitech.eu>
2019-10-31 11:13:10 +0100

    [AL][UPADD] Makefile, my.h & my_strcpy.h

---
+++ lib/my/my_strcpy.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** my_strcpy
+** File description:
+** 
+*/
+
+char *my_strcpy(char *dest ,char const *src)
+{
+    int i, j;
+    for (i = 0 ; src[i] != '\0' ; i++);
+    for (j = 0 ; j != i + 1 ; j++)
+    {
+        dest[j] = src[j];
+    }
+    return (dest);
+}
