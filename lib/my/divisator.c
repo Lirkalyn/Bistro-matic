6aaa66879cb4538297ee7ed1daf6cbcebf9d1ab8
Alexandre <alexandre1.berthomier@epitech.eu>
2019-10-31 08:12:33 +0100

    [AL][UPADD] Makefile & divisator.c

---
+++ lib/my/divisator.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** divisator
+** File description:
+** Ceci va probablement gérer les divisions
+*/
+
+#include <stdlib.h>
+#include "eval_expr.h"
+#include "my.h"
+
+char *el_multiplicator(char *num1, char *num2)
+{
+}
