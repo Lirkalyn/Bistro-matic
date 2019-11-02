a47e7c1e049edcd722ef480c48a6b5e9a67b0a8d
Alexandre <alexandre1.berthomier@epitech.eu>
2019-11-02 17:14:47 +0100

    [UPDATE] soustractor.c

--- lib/my/soustractor2.c
+++ lib/my/soustractor2.c
@@ -11,6 +11,7 @@
 
 char *num1_and_num2_neg(char *num1, char *num2, char *result)
 {
+    char *clean_nbr = malloc(sizeof(char) * my_strlen(result) + 1);
     char *tmp_n1 = malloc(sizeof(char) * my_strlen(num1) + 1);
     char *tmp_n2 = malloc(sizeof(char) * my_strlen(num2) + 1);
 
@@ -19,9 +20,16 @@ char *num1_and_num2_neg(char *num1, char *num2, char *result)
     tmp_n1[0] = '0';
     tmp_n2[0] = '0';
     result = el_soustractor(tmp_n2, tmp_n1);
-    if (nbr1_smaller(num1, num2) == 1)
+    if (nbr1_smaller(num1, num2) == 1 && my_strlen(num1) <= my_strlen(num2))
         result[0] = '0';
     else result[0] = '-';
+    if (result[1] == '0') {
+        for (int i = 0, j = 1 ; result[j] != '\0' ; i++, j++) {
+            clean_nbr[i] = result[j];
+        } if (result[0] == '-')
+            clean_nbr[0] = '-';
+        result = clean_nbr;
+    }
     return (result);
 }
 
