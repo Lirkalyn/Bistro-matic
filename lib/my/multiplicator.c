3a93e8fc951550e8b0abe5e754d8a176a2998015
Alexandre <alexandre1.berthomier@epitech.eu>
2019-11-02 14:42:28 +0100

    [UPDATE] multiplicator.c

--- lib/my/multiplicator.c
+++ lib/my/multiplicator.c
@@ -17,10 +17,10 @@ char *el_multiplicator(char *num1, char *num2)
     int tmp = 0;
 
     if ( my_strlen(num1) > my_strlen(num2) )
-        res = malloc(sizeof(char *) * my_strlen(num1) + 1);
-    else res = malloc(sizeof(char *) * my_strlen(num2) + 1);
+        res = malloc(sizeof(char *) * my_strlen(num1) + 2);
+    else res = malloc(sizeof(char *) * my_strlen(num2) + 2);
     for ( int i = my_strlen(num1) - 1 ; i != 0 ; i--, l++ ) {
-        res[l] = malloc(sizeof(char *) * my_strlen(num1) + my_strlen(num2) + 1);
+        res[l] = malloc(sizeof(char *) * my_strlen(num1) + my_strlen(num2) + 2);
         for ( int k = 0 ; k != l ; k++ )
             res[l][k] = '0';
         for ( int j = my_strlen(num2) - 1, c = l ; j != 0 ; j--, c++ ) {
@@ -41,7 +41,7 @@ char *el_additionator(int l, char **res, char *num1, char *num2)
     int k = -1;
     char *result;
 
-    result = malloc(sizeof(res[l]) + 1);
+    result = malloc(sizeof(char) * my_strlen(res[l]) + 2);
     for ( int i = 0 ; res[l][i] != '\0' ; i++ ) {
         tmp = 0;
         for ( int j = l ; j != k && res[j][i] != '\0' ; j-- ) {
